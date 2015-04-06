eeca3ae8ca5f9e74f2bec92ed08588d2a4295bdd
Christopher Vermilya <chrisverm@comcast.net>
2015-04-06 15:34:55 -0400

    Basic Camera implementation

--- KVEngine/Source/Engine/Camera.cpp
+++ KVEngine/Source/Engine/Camera.cpp
@@ -1,17 +1,59 @@
 #include "Camera.h"
+#include "Window.h"
 #include <iostream>
 
 namespace KVE
 {
+	using namespace DirectX;
+
 	Camera::Camera( const CameraParams& params )
 	{
 		fieldOfView = params.fieldOfView;
 		nearPlane = params.nearPlane;
 		farPlane = params.farPlane;
+
+		position = XMVectorSet( 0.0f, 0.0f, 0.0f, 1.0f );
+		scale = XMVectorSet( 1.0f, 1.0f, 1.0f, 1.0f );
+		orientation.forward = XMVectorSet( 0.0f, 0.0f, 1.0f, 0.0f );
+		orientation.up = XMVectorSet( 0.0f, 1.0f, 0.0f, 0.0f );
+		orientation.right = XMVectorSet( 1.0f, 0.0f, 0.0f, 0.0f );
+
+		projMatrix = DirectX::XMFLOAT4X4();
+		viewMatrix = DirectX::XMFLOAT4X4();
+
+		setProjMatrix();
+		setViewMatrix();
+	}
+
+	const XMFLOAT4X4 Camera::getProjMatrix( void ) const
+	{
+		return projMatrix;
+	}
+
+	const XMFLOAT4X4 Camera::getViewMatrix( void ) const
+	{
+		return viewMatrix;
 	}
 
 	void Camera::debugPrint( void )
 	{
 		printf( "Camera:\n  fieldOfView=%f\n  nearPlane=%f\n  farPlane=%f\n", fieldOfView, nearPlane, farPlane );
+	}
+
+	void Camera::setProjMatrix( void )
+	{
+		assert( farPlane > nearPlane );
+
+		XMStoreFloat4x4( &projMatrix, XMMatrixTranspose( XMMatrixPerspectiveFovLH(
+			fieldOfView, Window::Instance().getAspectRatio(), nearPlane, farPlane ) ) );
+	}
+
+	void Camera::setViewMatrix( void )
+	{
+		XMVECTOR eye = position - orientation.forward;
+		XMVECTOR focus = eye + orientation.forward;
+
+		XMStoreFloat4x4( &viewMatrix, XMMatrixTranspose( XMMatrixLookAtLH(
+			eye, focus, orientation.up ) ) );
 	}
 }
--- KVEngine/Source/Engine/Camera.h
+++ KVEngine/Source/Engine/Camera.h
@@ -1,10 +1,18 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <DirectXMath.h>
 #include "DataTypes.h"
 
 namespace KVE
 {
+	struct Orientation
+	{
+		DirectX::XMVECTOR forward;
+		DirectX::XMVECTOR up;
+		DirectX::XMVECTOR right;
+	};
+
 	struct CameraParams
 	{
 		f32 fieldOfView;
@@ -17,6 +25,9 @@ namespace KVE
 	public:
 		Camera( const CameraParams& params );
 
+		const DirectX::XMFLOAT4X4 getProjMatrix( void )const;
+		const DirectX::XMFLOAT4X4 getViewMatrix( void )const;
+
 		void debugPrint( void );
 
 	private:
@@ -24,10 +35,15 @@ namespace KVE
 		f32 nearPlane;
 		f32 farPlane;
 
-		/* pseudo-code
-		vector3* mountPosition;
-		vector3* mountRotation;
-		*/
+		DirectX::XMVECTOR position;
+		Orientation orientation;
+		DirectX::XMVECTOR scale;
+
+		DirectX::XMFLOAT4X4 projMatrix;
+		DirectX::XMFLOAT4X4 viewMatrix;
+
+		void setProjMatrix( void );
+		void setViewMatrix( void );
 
 	};
 }
