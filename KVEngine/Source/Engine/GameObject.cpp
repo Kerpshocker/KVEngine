6b32f83b9ca3c651cb7ac1ab9cf514b8e079febb
Nathaniel Kierpiec <nate.kierpiec@gmail.com>
2015-04-12 13:46:52 -0400

    Created GameObject
    Added basic functionality for a game object

---
+++ KVEngine/Source/Engine/GameObject.cpp
@@ -0,0 +1,73 @@
+#include "GameObject.h"
+#include "Mesh.h"
+#include "Material.h"
+
+using namespace DirectX;
+
+namespace KVE
+{
+	GameObject::GameObject( void )
+	{
+
+	}
+
+	GameObject::GameObject( Mesh gMesh, Material gMaterial )
+	{
+		mesh = gMesh;
+		material = gMaterial;
+	}
+
+	GameObject::~GameObject()
+	{
+	}
+
+	void GameObject::transformWorldMatrix( float dt )
+	{
+		//do some calculations on whichever pos vector based on dt
+	}
+
+	XMFLOAT4X4 GameObject::getWorldMatrix( void )
+	{
+		return world;
+	}
+
+	Vector GameObject::getPosition( void )
+	{
+		return position;
+	}
+
+	Vector GameObject::getRotation( void )
+	{
+		return rotation;
+	}
+
+	Vector GameObject::getScale( void )
+	{
+		return scale;
+	}
+
+	void GameObject::setPostion( Vector vPosition )
+	{
+		position = vPosition;
+	}
+
+	void GameObject::setPostion( Vector vRotation )
+	{
+		rotation = vRotation;
+	}
+
+	void GameObject::setPostion( Vector vScale )
+	{
+		scale = vScale;
+	}
+
+	void GameObject::saveMesh( Mesh gMesh )
+	{
+		mesh = gMesh;
+	}
+
+	void GameObject::saveMaterial( Material gMaterial )
+	{
+		material = gMaterial;
+	}
+}
---
+++ KVEngine/Source/Engine/GameObject.h
@@ -0,0 +1,49 @@
+#ifndef GAME_OBJECT_H
+#define GAME_OBJECT_H
+
+#include <DirectXMath.h>
+#include "DataTypes.h"
+
+namespace KVE
+{
+	struct Vector
+	{
+		f32					x;
+		f32					y;
+		f32					z;
+	};
+
+	class GameObject
+	{
+	public:
+		Mesh				mesh;
+		Material			material;
+
+	public:
+		GameObject( void );
+		GameObject( Mesh mesh, Material material );
+		~GameObject();
+
+		void transformWorldMatrix( float deltaTime );
+
+		DirectX::XMFLOAT4X4 getWorldMatrix( void );
+		Vector getPosition( void );
+		Vector getRotation( void );
+		Vector getScale( void );
+
+		void setPostion( Vector position );
+		void setRotation( Vector rotation );
+		void setScale( Vector scale );
+
+		void saveMesh( Mesh mesh );
+		void saveMaterial( Material material );
+
+	private:
+		DirectX::XMFLOAT4X4 world;
+		Vector				position;
+		Vector				rotation;
+		Vector				scale;
+	};
+}
+
+#endif
