ed409ed091c2c75d0fad5a760dd6bdad51be4b1d
Nathaniel Kierpiec <nate.kierpiec@gmail.com>
2015-05-07 11:14:32 -0400

    Cleaned up a little bit in OBB file
    in the process of changing XMFLOAT3 to XMVECTOR

--- KVEngine/Source/Engine/OBB.cpp
+++ KVEngine/Source/Engine/OBB.cpp
@@ -2,6 +2,8 @@
 
 #include <math.h>
 
+using namespace DirectX;
+
 namespace KVE
 {
 	namespace Collisions
@@ -10,25 +12,25 @@ namespace KVE
 		{
 		}
 
-		OBB::OBB( DirectX::XMFLOAT3 vPosition, DirectX::XMFLOAT3 vFrontTopRight, DirectX::XMFLOAT3 vBackBottomLeft )
+		OBB::OBB( XMFLOAT3 vPosition, XMFLOAT3 vFrontTopRight, XMFLOAT3 vBackBottomLeft )
 		{
 			position = vPosition;
-			frontTopRight = vFrontTopRight;
-			frontTopLeft = DirectX::XMFLOAT3{ backBottomLeft.x, frontTopRight.y, frontTopRight.z };
-			frontBottomRight = DirectX::XMFLOAT3{ frontTopRight.x, backBottomLeft.y, frontTopRight.z };
-			frontBottomLeft = DirectX::XMFLOAT3{ backBottomLeft.x, backBottomLeft.y, frontTopRight.z };
+			corners.frontTopRight = vFrontTopRight;
+			corners.frontTopLeft = XMFLOAT3{ corners.backBottomLeft.x, corners.frontTopRight.y, corners.frontTopRight.z };
+			corners.frontBottomRight = XMFLOAT3{ corners.frontTopRight.x, corners.backBottomLeft.y, corners.frontTopRight.z };
+			corners.frontBottomLeft = XMFLOAT3{ corners.backBottomLeft.x, corners.backBottomLeft.y, corners.frontTopRight.z };
 
-			backTopRight = DirectX::XMFLOAT3{ frontTopRight.x, frontTopRight.y, backBottomLeft.z };
-			backTopLeft = DirectX::XMFLOAT3{ backBottomLeft.x, frontTopRight.y, backBottomLeft.z };
-			backBottomRight = DirectX::XMFLOAT3{ frontTopRight.x, backBottomLeft.y, backBottomLeft.z };
-			backBottomLeft = vBackBottomLeft;
+			corners.backTopRight = XMFLOAT3{ corners.frontTopRight.x, corners.frontTopRight.y, corners.backBottomLeft.z };
+			corners.backTopLeft = XMFLOAT3{ corners.backBottomLeft.x, corners.frontTopRight.y, corners.backBottomLeft.z };
+			corners.backBottomRight = XMFLOAT3{ corners.frontTopRight.x, corners.backBottomLeft.y, corners.backBottomLeft.z };
+			corners.backBottomLeft = vBackBottomLeft;
 
-			scale = DirectX::XMFLOAT3{ 1.0f, 1.0f, 1.0f };
-			rotation = DirectX::XMFLOAT3{ 0.0f, 0.0f, 0.0f };
+			scale = XMFLOAT3{ 1.0f, 1.0f, 1.0f };
+			rotation = XMFLOAT3{ 0.0f, 0.0f, 0.0f };
 
-			width = abs( frontTopRight.x - backBottomLeft.x );
-			height = abs( frontTopRight.y - backBottomLeft.y );
-			depth = abs( frontTopRight.z - backBottomLeft.z );
+			width = abs( corners.frontTopRight.x - corners.backBottomLeft.x );
+			height = abs( corners.frontTopRight.y - corners.backBottomLeft.y );
+			depth = abs( corners.frontTopRight.z - corners.backBottomLeft.z );
 
 			UpdateCollisionPoints( );
 		}
@@ -37,22 +39,48 @@ namespace KVE
 		{
 		}
 
-		void OBB::setPosition( DirectX::XMFLOAT3 vPosition )
+		void OBB::setPosition( XMFLOAT3 vPosition )
 		{
 			position = vPosition;
 			UpdateCollisionPoints( );
 		}
 
-		void OBB::setRotation( DirectX::XMFLOAT3 vRotation )
+		void OBB::setRotation( XMFLOAT3 vRotation )
 		{
 			position = vRotation;
 			UpdateCollisionPoints( );
 		}
 
-		void OBB::setScale( DirectX::XMFLOAT3 vScale )
+		void OBB::setScale( XMFLOAT3 vScale )
 		{
 			position = vScale;
 			UpdateCollisionPoints( );
+		}
+
+		XMVECTOR OBB::getSurfaceNormal( XMVECTOR vert1, XMVECTOR vert2, XMVECTOR vert3 )
+		{
+			//create edges from verts
+			XMVECTOR normal;
+
+			XMVECTOR vert1SubVert2 = XMVectorSet(
+					XMVectorGetIntX( vert1 ) - XMVectorGetIntX( vert2 ),
+					XMVectorGetIntY( vert1 ) - XMVectorGetIntY( vert2 ),
+					XMVectorGetIntZ( vert1 ) - XMVectorGetIntZ( vert2 ),
+					XMVectorGetIntW( vert1 ) - XMVectorGetIntW( vert2 )
+				);
+
+			XMVECTOR vert3SubVert2 = XMVectorSet(
+					XMVectorGetIntX( vert3 ) - XMVectorGetIntX( vert2 ),
+					XMVectorGetIntY( vert3 ) - XMVectorGetIntY( vert2 ),
+					XMVectorGetIntZ( vert3 ) - XMVectorGetIntZ( vert2 ),
+					XMVectorGetIntW( vert3 ) - XMVectorGetIntW( vert2 )
+				);
+			
+			normal = XMVector3Cross( vert1SubVert2, vert3SubVert2 );
+
+			XMVector3Normalize( normal );
+
+			return normal;
 		}
 	}
 }
--- KVEngine/Source/Engine/OBB.h
+++ KVEngine/Source/Engine/OBB.h
@@ -7,6 +7,30 @@ namespace KVE
 {
 	namespace Collisions
 	{
+		struct OABBCorners
+		{
+			DirectX::XMFLOAT3 frontTopRight;				//front top right corner of OABB
+			DirectX::XMFLOAT3 frontTopLeft;					//front top left corner of OABB
+			DirectX::XMFLOAT3 frontBottomRight;				//front bottom right corner of OABB
+			DirectX::XMFLOAT3 frontBottomLeft;				//front bottom left corner of OABB
+			DirectX::XMFLOAT3 backTopRight;					//back top right corner of OABB
+			DirectX::XMFLOAT3 backTopLeft;					//back top left corner of OABB
+			DirectX::XMFLOAT3 backBottomRight;				//back bottom right corner of OABB
+			DirectX::XMFLOAT3 backBottomLeft;				//back bottom left corner of OABB
+		};
+
+		struct OABBCollisionCorners
+		{
+			DirectX::XMFLOAT3 collisionFrontTopRight;		//translated top top right corner of OABB used for collisions
+			DirectX::XMFLOAT3 collisionFrontTopLeft;		//translated top top left corner of OABB used for collisions
+			DirectX::XMFLOAT3 collisionFrontBottomRight;	//translated top bottom right corner of OABB used for collisions
+			DirectX::XMFLOAT3 collisionFrontBottomLeft;		//translated top bottom left corner of OABB used for collisions
+			DirectX::XMFLOAT3 collisionBackTopRight;		//translated back top right corner of OABB used for collisions
+			DirectX::XMFLOAT3 collisionBackTopLeft;			//translated back top left corner of OABB used for collisions
+			DirectX::XMFLOAT3 collisionBackBottomRight;		//translated back bottom right corner of OABB used for collisions
+			DirectX::XMFLOAT3 collisionBackBottomLeft;		//translated back bottom left corner of OABB used for collisions
+		};
+
 		class OBB : BoundingVolume
 		{
 		public:
@@ -14,14 +38,14 @@ namespace KVE
 			OBB( DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 frontTopRight, DirectX::XMFLOAT3 backBottomLeft );
 			~OBB( );
 
-			DirectX::XMFLOAT3 getCollisionFrontTopRight( void ){ return collisionFrontTopRight; };
-			DirectX::XMFLOAT3 getCollisionFrontTopLeft( void ){ return collisionFrontTopLeft; };
-			DirectX::XMFLOAT3 getCollisionFrontBottomRight( void ){ return collisionFrontBottomRight; };
-			DirectX::XMFLOAT3 getCollisionFrontBottomLeft( void ){ return collisionFrontBottomLeft; };
-			DirectX::XMFLOAT3 getCollisionBackTopRight( void ){ return collisionBackTopRight; };
-			DirectX::XMFLOAT3 getCollisionBackTopLeft( void ){ return collisionBackTopLeft; };
-			DirectX::XMFLOAT3 getCollisionBackBottomRight( void ){ return collisionBackBottomRight; };
-			DirectX::XMFLOAT3 getCollisionBackBottomLeft( void ){ return collisionBackBottomLeft; };
+			DirectX::XMFLOAT3 getCollisionFrontTopRight( void ){ return collisionCorners.collisionFrontTopRight; };
+			DirectX::XMFLOAT3 getCollisionFrontTopLeft( void ){ return collisionCorners.collisionFrontTopLeft; };
+			DirectX::XMFLOAT3 getCollisionFrontBottomRight( void ){ return collisionCorners.collisionFrontBottomRight; };
+			DirectX::XMFLOAT3 getCollisionFrontBottomLeft( void ){ return collisionCorners.collisionFrontBottomLeft; };
+			DirectX::XMFLOAT3 getCollisionBackTopRight( void ){ return collisionCorners.collisionBackTopRight; };
+			DirectX::XMFLOAT3 getCollisionBackTopLeft( void ){ return collisionCorners.collisionBackTopLeft; };
+			DirectX::XMFLOAT3 getCollisionBackBottomRight( void ){ return collisionCorners.collisionBackBottomRight; };
+			DirectX::XMFLOAT3 getCollisionBackBottomLeft( void ){ return collisionCorners.collisionBackBottomLeft; };
 			
 			DirectX::XMFLOAT3 getPosition( void ){ return position; };
 			DirectX::XMFLOAT3 getRotation( void ){ return rotation; };
@@ -39,32 +63,20 @@ namespace KVE
 			IntersectionValue intersects( BoundingVolume other );
 
 		private:
-			UINT indices;									//used for the drawing of the OABB
+			UINT					indices;	//used for the drawing of the OABB
 
-			DirectX::XMFLOAT3 frontTopRight;				//front top right corner of OABB
-			DirectX::XMFLOAT3 frontTopLeft;					//front top left corner of OABB
-			DirectX::XMFLOAT3 frontBottomRight;				//front bottom right corner of OABB
-			DirectX::XMFLOAT3 frontBottomLeft;				//front bottom left corner of OABB
-			DirectX::XMFLOAT3 backTopRight;					//back top right corner of OABB
-			DirectX::XMFLOAT3 backTopLeft;					//back top left corner of OABB
-			DirectX::XMFLOAT3 backBottomRight;				//back bottom right corner of OABB
-			DirectX::XMFLOAT3 backBottomLeft;				//back bottom left corner of OABB
+			OABBCorners				corners;
+			OABBCollisionCorners	collisionCorners;
 
-			DirectX::XMFLOAT3 collisionFrontTopRight;		//translated top top right corner of OABB used for collisions
-			DirectX::XMFLOAT3 collisionFrontTopLeft;		//translated top top left corner of OABB used for collisions
-			DirectX::XMFLOAT3 collisionFrontBottomRight;	//translated top bottom right corner of OABB used for collisions
-			DirectX::XMFLOAT3 collisionFrontBottomLeft;		//translated top bottom left corner of OABB used for collisions
-			DirectX::XMFLOAT3 collisionBackTopRight;		//translated back top right corner of OABB used for collisions
-			DirectX::XMFLOAT3 collisionBackTopLeft;			//translated back top left corner of OABB used for collisions
-			DirectX::XMFLOAT3 collisionBackBottomRight;		//translated back bottom right corner of OABB used for collisions
-			DirectX::XMFLOAT3 collisionBackBottomLeft;		//translated back bottom left corner of OABB used for collisions
-
-			f32 width;
-			f32 height;
-			f32 depth;
+			f32						width;
+			f32						height;
+			f32						depth;
 
 			//used to keep in sync with the characteristics of the enclosed object
 			void UpdateCollisionPoints( );
+
+			//returns the normal to a polygons surface, in which the polygon defined by three vectors
+			DirectX::XMVECTOR getSurfaceNormal( DirectX::XMVECTOR vert1, DirectX::XMVECTOR vert2, DirectX::XMVECTOR vert3 );
 		};
 	}
 }
