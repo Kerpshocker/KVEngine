82dff1f97944adb9fa992e3c5ea88b112407456c
Nathaniel Kierpiec <nate.kierpiec@gmail.com>
2015-05-07 11:42:14 -0400

    Fixed Conversion from float to int
    Now just used floats

--- KVEngine/Source/Engine/OBB.cpp
+++ KVEngine/Source/Engine/OBB.cpp
@@ -14,21 +14,21 @@ namespace KVE
 		{
 			m_Position = vPosition;
 			m_Corners.frontTopRight = vFrontTopRight;
-			m_Corners.frontTopLeft = XMVectorSet( XMVectorGetIntX( m_Corners.backBottomLeft ), XMVectorGetIntY( m_Corners.frontTopRight ), XMVectorGetIntZ( m_Corners.frontTopRight ), 0.0f );
-			m_Corners.frontBottomRight = XMVectorSet( XMVectorGetIntX( m_Corners.frontTopRight ), XMVectorGetIntY( m_Corners.backBottomLeft ), XMVectorGetIntZ( m_Corners.frontTopRight ), 0.0f );
-			m_Corners.frontBottomLeft = XMVectorSet( XMVectorGetIntX( m_Corners.backBottomLeft ), XMVectorGetIntY( m_Corners.backBottomLeft ), XMVectorGetIntZ( m_Corners.frontTopRight ), 0.0f );
+			m_Corners.frontTopLeft = XMVectorSet( XMVectorGetX( m_Corners.backBottomLeft ), XMVectorGetY( m_Corners.frontTopRight ), XMVectorGetZ( m_Corners.frontTopRight ), 0.0f );
+			m_Corners.frontBottomRight = XMVectorSet( XMVectorGetX( m_Corners.frontTopRight ), XMVectorGetY( m_Corners.backBottomLeft ), XMVectorGetZ( m_Corners.frontTopRight ), 0.0f );
+			m_Corners.frontBottomLeft = XMVectorSet( XMVectorGetX( m_Corners.backBottomLeft ), XMVectorGetY( m_Corners.backBottomLeft ), XMVectorGetZ( m_Corners.frontTopRight ), 0.0f );
 
-			m_Corners.backTopRight = XMVectorSet( XMVectorGetIntX( m_Corners.frontTopRight ), XMVectorGetIntY( m_Corners.frontTopRight ), XMVectorGetIntZ( m_Corners.backBottomLeft ), 0.0f );
-			m_Corners.backTopLeft = XMVectorSet( XMVectorGetIntX( m_Corners.backBottomLeft ), XMVectorGetIntY( m_Corners.frontTopRight ), XMVectorGetIntZ( m_Corners.backBottomLeft ), 0.0f );
-			m_Corners.backBottomRight = XMVectorSet( XMVectorGetIntX( m_Corners.frontTopRight ), XMVectorGetIntY( m_Corners.backBottomLeft ), XMVectorGetIntZ( m_Corners.backBottomLeft ), 0.0f );
+			m_Corners.backTopRight = XMVectorSet( XMVectorGetX( m_Corners.frontTopRight ), XMVectorGetY( m_Corners.frontTopRight ), XMVectorGetZ( m_Corners.backBottomLeft ), 0.0f );
+			m_Corners.backTopLeft = XMVectorSet( XMVectorGetX( m_Corners.backBottomLeft ), XMVectorGetY( m_Corners.frontTopRight ), XMVectorGetZ( m_Corners.backBottomLeft ), 0.0f );
+			m_Corners.backBottomRight = XMVectorSet( XMVectorGetX( m_Corners.frontTopRight ), XMVectorGetY( m_Corners.backBottomLeft ), XMVectorGetZ( m_Corners.backBottomLeft ), 0.0f );
 			m_Corners.backBottomLeft = vBackBottomLeft;
 
 			m_Scale = &XMVectorSet( 1.0f, 1.0f, 1.0f, 0.0f );
 			m_Rotation = &XMVectorSet( 0.0f, 0.0f, 0.0f, 0.0f );
 
-			m_Width = XMVectorGetIntX( m_Corners.frontTopRight ) - XMVectorGetIntX( m_Corners.backBottomLeft );
-			m_Height = XMVectorGetIntY( m_Corners.frontTopRight ) - XMVectorGetIntY( m_Corners.backBottomLeft );
-			m_Depth = XMVectorGetIntZ( m_Corners.frontTopRight ) - XMVectorGetIntZ( m_Corners.backBottomLeft );
+			m_Width = XMVectorGetX( m_Corners.frontTopRight ) - XMVectorGetX( m_Corners.backBottomLeft );
+			m_Height = XMVectorGetY( m_Corners.frontTopRight ) - XMVectorGetY( m_Corners.backBottomLeft );
+			m_Depth = XMVectorGetZ( m_Corners.frontTopRight ) - XMVectorGetZ( m_Corners.backBottomLeft );
 
 			UpdateCollisionPoints();
 		}
@@ -61,17 +61,17 @@ namespace KVE
 			XMVECTOR normal;
 
 			XMVECTOR vert1SubVert2 = XMVectorSet(
-				XMVectorGetIntX( vert1 ) - XMVectorGetIntX( vert2 ),
-				XMVectorGetIntY( vert1 ) - XMVectorGetIntY( vert2 ),
-				XMVectorGetIntZ( vert1 ) - XMVectorGetIntZ( vert2 ),
-				XMVectorGetIntW( vert1 ) - XMVectorGetIntW( vert2 )
+				XMVectorGetX( vert1 ) - XMVectorGetX( vert2 ),
+				XMVectorGetY( vert1 ) - XMVectorGetY( vert2 ),
+				XMVectorGetZ( vert1 ) - XMVectorGetZ( vert2 ),
+				XMVectorGetW( vert1 ) - XMVectorGetW( vert2 )
 				);
 
 			XMVECTOR vert3SubVert2 = XMVectorSet(
-				XMVectorGetIntX( vert3 ) - XMVectorGetIntX( vert2 ),
-				XMVectorGetIntY( vert3 ) - XMVectorGetIntY( vert2 ),
-				XMVectorGetIntZ( vert3 ) - XMVectorGetIntZ( vert2 ),
-				XMVectorGetIntW( vert3 ) - XMVectorGetIntW( vert2 )
+				XMVectorGetX( vert3 ) - XMVectorGetX( vert2 ),
+				XMVectorGetY( vert3 ) - XMVectorGetY( vert2 ),
+				XMVectorGetZ( vert3 ) - XMVectorGetZ( vert2 ),
+				XMVectorGetW( vert3 ) - XMVectorGetW( vert2 )
 				);
 
 			normal = XMVector3Cross( vert1SubVert2, vert3SubVert2 );
